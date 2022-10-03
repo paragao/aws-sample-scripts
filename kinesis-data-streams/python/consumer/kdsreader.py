88f2bca394cf6fc85baa8ddbda6aabfa5fb30923
Paulo Aragao <aragao.paulo@gmail.com>
2022-10-03 16:32:41 -0300

    changed to batch_writer

--- kinesis-data-streams/python/consumer/kdsreader.py
+++ kinesis-data-streams/python/consumer/kdsreader.py
@@ -15,27 +15,28 @@ table = ddb.Table('kinesis-ingest')
 
 @xray_recorder.capture('lambda main parsing kinesis data')
 def lambda_handler(event, context):
-    for record in event['Records']:
-        preDec = b64decode(record['kinesis']['data'])
-        dec = json.loads(str(b64decode(preDec), "ascii"))
+    with table.batch_writer() as batch:
+        for record in event['Records']:
+            preDec = b64decode(record['kinesis']['data'])
+            dec = json.loads(str(b64decode(preDec), "ascii"))
 
-        params = {
-            'TableName': 'kinesis-ingest',
-            'kinesisPartitionKey': record['kinesis']['partitionKey'],
-            'datetime': dec['date'],
-            'transaction_value': dec['transaction_value'],
-            'credit_card_number': dec['credit_card_number'],
-            'account_iban': dec['account_iban'],
-            'bank_country': dec['bank_country'],
-            'country': dec['country'],
-            'name': dec['name'],
-            'payload_full': dec,
-        }
+            params = {
+                'TableName': 'kinesis-ingest',
+                'kinesisPartitionKey': record['kinesis']['partitionKey'],
+                'datetime': dec['date'],
+                'transaction_value': dec['transaction_value'],
+                'credit_card_number': dec['credit_card_number'],
+                'account_iban': dec['account_iban'],
+                'bank_country': dec['bank_country'],
+                'country': dec['country'],
+                'name': dec['name'],
+                'payload_full': dec,
+            }
 
-        response = table.put_item(
-            Item=params
-        )
+            batch.put_item(
+                Item=params
+            )
 
-        logger.info('item inserted \r', response)
+    logger.info('batch items inserted \r')
 
     return 'data processed'
